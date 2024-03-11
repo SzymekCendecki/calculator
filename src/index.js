2e6dcffefd6a811b433f5e7aa1000734ba5f4bea
Szymon Cendecki <31997216+SzymekCendecki@users.noreply.github.com>
2024-03-11 17:17:15 +0100

    Add files via upload

--- src/index.js
+++ src/index.js
@@ -19,6 +19,38 @@ document.addEventListener("DOMContentLoaded", ()=>{
       temp = 2;
     })
 
+    document.querySelector("#three").addEventListener("click", ()=>{
+      temp = 3;
+    })
+
+    document.querySelector("#four").addEventListener("click", ()=>{
+      temp = 4;
+    })
+
+    document.querySelector("#five").addEventListener("click", ()=>{
+      temp = 5;
+    })
+
+    document.querySelector("#six").addEventListener("click", ()=>{
+      temp = 6;
+    })
+
+    document.querySelector("#seven").addEventListener("click", ()=>{
+      temp = 7;
+    })
+
+    document.querySelector("#eight").addEventListener("click", ()=>{
+      temp = 8;
+    })
+
+    document.querySelector("#nine").addEventListener("click", ()=>{
+      temp = 9;
+    })
+
+    document.querySelector("#zero").addEventListener("click", ()=>{
+      temp = 0;
+    })
+    
 
 
 
