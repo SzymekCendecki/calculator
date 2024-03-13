d19d18593fc0afa86b53716669cb0d3ffdba1433
Szymon Cendecki <31997216+SzymekCendecki@users.noreply.github.com>
2024-03-13 20:13:34 +0100

    Add files via upload

--- src/index.js
+++ src/index.js
@@ -3,74 +3,37 @@ import "./style.scss";
 document.addEventListener("DOMContentLoaded", ()=>{ 
     console.log("DOM fully loaded and parsed!")
 
-
-    let temp = 0;
-    let result  = 0;
+    let result = 0;
+    let tab = [];
 
     document.querySelector("#result").innerHTML = result;
 
 
-    document.querySelector("#one").addEventListener("click", ()=>{
-      temp = 1;
-    })
+document.querySelector("#plus").addEventListener("click", ()=>{
+  let x = parseFloat(document.querySelector("#number").value);
+  
+  tab.push(x)
+
+  let sum = tab.reduce((accu, value) => accu + value); 
+  document.querySelector("#result").innerHTML = sum;
 
 
-    document.querySelector("#two").addEventListener("click", ()=>{
-      temp = 2;
-    })
-
-    document.querySelector("#three").addEventListener("click", ()=>{
-      temp = 3;
-    })
-
-    document.querySelector("#four").addEventListener("click", ()=>{
-      temp = 4;
-    })
-
-    document.querySelector("#five").addEventListener("click", ()=>{
-      temp = 5;
-    })
-
-    document.querySelector("#six").addEventListener("click", ()=>{
-      temp = 6;
-    })
-
-    document.querySelector("#seven").addEventListener("click", ()=>{
-      temp = 7;
-    })
-
-    document.querySelector("#eight").addEventListener("click", ()=>{
-      temp = 8;
-    })
-
-    document.querySelector("#nine").addEventListener("click", ()=>{
-      temp = 9;
-    })
-
-    document.querySelector("#zero").addEventListener("click", ()=>{
-      temp = 0;
-    })
-    
-
-
-
- document.querySelector("#plus").addEventListener("click", ()=>{
-  result = result + temp;
-  document.querySelector("#result").innerHTML = result;
- 
  })
 
- document.querySelector("#minus").addEventListener("click", ()=>{
-  result = result - temp;
-  document.querySelector("#result").innerHTML = result;
- 
+ document.querySelector("#multiply").addEventListener("click", ()=>{
+  let x = parseFloat(document.querySelector("#number").value);
+  
+  tab.push(x)
+  console.log(tab)
+
+  let sum = tab.reduce((accu, value) => accu * value); 
+  document.querySelector("#result").innerHTML = sum;
+  
  })
 
 
  document.querySelector("#delete").addEventListener("click", ()=>{
-  temp = 0;
-  result = 0;
-  document.querySelector("#result").innerHTML = result;
+
  })
 
 
